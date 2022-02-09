bfaf3741f0ecb11c0c6ccd30cdc9a67563563c47
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-02-09 05:33:19 +0100

    Modified JSX Component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+const { registerBlockType } = wp.blocks;
+
 const edit = () => {
 	return (
 		<div>Hello World</div>
 	)
 }
+
+registerBlockType('blockify/blurb', {
+	title: 'Blurb',
+	description: 'A Blockify Blurb Block',
+	icon: 'smiley',
+	category: 'layout',
+	attributes: {},
+	edit: edit,
+	save: save,
+})
