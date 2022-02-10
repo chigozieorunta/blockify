3846d0df8d2638e88a63d56d860c0ced0159380b
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-02-10 18:08:46 +0100

    Registered custom compiled ES5 script for component and React Bootstrap

--- inc/Plugin.php
+++ inc/Plugin.php
@@ -34,7 +34,9 @@ class Plugin {
 	 * @return void
 	 */
 	public function register_blockify() {
-		wp_register_script( 'blockify-blurb', plugin_dir_url( __FILE__ ) . 'build/index.js?version' . time(), array( 'wp-blocks' ) );
+		wp_enqueue_style( 'bootstrap', plugin_dir_url( __FILE__ ) . '../assets/css/bootstrap.min.css' );
+
+		wp_register_script( 'blockify-blurb', plugin_dir_url( __FILE__ ) . '../build/index.js', array( 'wp-blocks', 'wp-editor' ) );
 
 		register_block_type( 'blockify/blurb', array(
 			'editor_script' => 'blockify-blurb',
