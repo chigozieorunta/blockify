83d6e015262968ed5fdbd5c901f22a828470aec2
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-02-11 01:21:20 +0100

    Changed save function and replaced elements with RichText component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,60 @@
 const { registerBlockType } = wp.blocks;
+const { RichText } = wp.editor;
+const { Container, Row, Col } = require('react-bootstrap');
 
-const edit = () => {
+import './index.css';
+
+const edit = ({ attributes, setAttributes }) => {
+
+	const { title, body } = attributes;
+
+	const updateTitle = newTitle => {
+		setAttributes({ title: newTitle })
+	}
+
+	const updateBody = newBody => {
+		setAttributes({ body: newBody });
+	}
+
 	return (
-		<div>Hello World</div>
-	)
+		<section className="text-center">
+			<Container>
+				<Row>
+					<Col sm={{ span: 6, offset: 3 }}>
+						<RichText
+							key="editable"
+							tagName="h2"
+							placeholder="Blurb Title"
+							value={title}
+							onChange={updateTitle}
+						/>
+						<RichText
+							key="editable"
+							tagName="p"
+							placeholder="Blurb Body"
+							value={body}
+							onChange={updateBody}
+						/>
+					</Col>
+				</Row>
+			</Container>
+		</section>
+	);
+}
+
+const save = ({ attributes }) => {
+	return (
+		<section className="text-center">
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-6 offset-sm-3">
+						<h4>{attributes.title}</h4>
+						<p>{attributes.body}</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 registerBlockType('blockify/blurb', {
@@ -11,7 +62,18 @@ registerBlockType('blockify/blurb', {
 	description: 'A Blockify Blurb Block',
 	icon: 'smiley',
 	category: 'layout',
-	attributes: {},
+	attributes: {
+		title: {
+			type: 'string',
+			source: 'html',
+			selector: 'h2'
+		},
+		body: {
+			type: 'string',
+			source: 'html',
+			selector: 'p'
+		},
+	},
 	edit: edit,
 	save: save,
 })
